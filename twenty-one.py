138bc8b8c420326e6a2e2442fe8a10baa88426ab
Dmitriy <24dmitriy@mail.ru>
2020-08-28 01:47:12 +0300

    Some improvements in programm. Created func that summ points

--- twenty-one.py
+++ twenty-one.py
@@ -1,20 +1,44 @@
-from random import randint, choice
+from random import choice
 
 from deck import deck
 
 
+# get users_card, return summ of the card
+def summ_card(**users_deck):
+    print(users_deck)
+    points = 0
+    for number in users_deck.values():
+        points += int(number)
+    print('summ = ', points)
 
     
-# get random card from deck
+# get random card from deck (return dict)
 card = choice(list(deck.items()))
-print(card)
-
 # delete chosen card from deck
 deck.pop(card[0])
-print(deck)
+# create new dict of users card
+users_card = {}
+# add card to users dict
+users_card[card[0]] = card[1]
+print(users_card)
+
+
+
+# get random card from deck (return dict)
+card = choice(list(deck.items()))
+# delete chosen card from deck
+deck.pop(card[0])
+# add card to users dict
+users_card[card[0]] = card[1]
+print(users_card)
 
 
 
 
 
+summ_card(**users_card)
 
+
+
+#print('your cards: ', card[0])
+#print('your summ is: ', card[1])

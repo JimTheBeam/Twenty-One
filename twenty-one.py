16fa23daf37e96cc13720ad4d197b749689e7647
Dmitriy <24dmitriy@mail.ru>
2020-08-29 00:20:47 +0300

    полностью переделал логику

--- twenty-one.py
+++ twenty-one.py
@@ -3,64 +3,46 @@ from random import choice
 from deck import deck
 
 
-# create new dict of users card
-users_card = {}
-
-
-# get users_card, return summ of the card
-def summ_card(**users_deck):
-    points = 0
-    for number in users_deck.values():
-        points += (number)
-    return points
+# create new list of users card
+users_deck = []
 
 
 # get random card from deck (return dict)
 def rand_card(**deck):
-    card = choice(list(deck.items()))
+    card = choice(list(deck.keys()))
     return card
-#print('рандомная карта проверка функции')
-#print(rand_card(**deck))
 
 
-# delete chosen card(in rand_card) from deck
-def del_card_from_deck(*card, **deck):
-    card = card[0]
-    deck.pop(card[0])
-    return deck
+# check if random card is in users deck. Return True if IN
+def if_card_in_udeck(card, *users_deck):
+    if card in users_deck:
+        return True
+    else:
+        return False
 
 
-def add_user_card(card):
-    users_card[card[0]] = card[1]
-    return users_card
-
-
-
-
-def start(card, **deck):
-    
-    for i in range(1, 3):
+# add random card in users deck. Return list of users card
+def add_newcard_in_udeck(*users_deck, **deck):
+    users_deck = list(users_deck)
+    while True:
         card = rand_card(**deck)
-        deck = del_card_from_deck(card, **deck)
-        user_card = add_user_card(card)
-
-    # print user's deck as a list
-    a = []
-    for j in user_card:
-        a.append(j)
-    print(a)
+        if if_card_in_udeck(card, *users_deck):
+            continue
+        else:
+            break
+    users_deck.append(card)
+    return users_deck
 
 
-def game(card, **deck):
-    newcard = rand_card(**deck)
-    deck = del_card_from_deck(newcard, **deck)
-    user_card = add_user_card(newcard)
-
-    # print user's deck as a list
-    a = []
-    for j in user_card:
-        a.append(j)
-    print(a)
+# get users_deck, return summ of the card
+def summ_card(*users_deck, **deck):
+    points = 0
+    for name in users_deck:
+        if deck.get(name) == None:
+            pass
+        else:
+            points += deck.get(name)
+    return points
 
 
 
@@ -75,8 +57,7 @@ def check_win(summ_card):
 
 
 
-def add_card(**deck):
-    print(deck)
+
 
 
 
@@ -86,24 +67,27 @@ def add_card(**deck):
 if __name__ == '__main__':
 
     print('Game started \nYou get: ')
+    users_deck = add_newcard_in_udeck(*users_deck, **deck)
+    users_deck = add_newcard_in_udeck(*users_deck, **deck)  
+    print(users_deck)
+    
 
-    card = rand_card(**deck)
-    start(card, **deck)
+    points = summ_card(*users_deck, **deck)
 
-    running = True
+    # running = True
 
-    while running:
+    # while running:
         
-        points = summ_card(**users_card)
-        print("Your summ = ", points)
+    #     points = summ_card(*users_deck)
+    #     print("Your summ = ", points)
 
-        if points == 21:
-            print('Congratulation you win!')
-            break
-        elif points > 21:
-            print('You loose')
-            break
-        else:
-            print('Want to get one more card?')
-            game(card, **deck)
+    #     if points == 21:
+    #         print('Congratulation you win!')
+    #         break
+    #     elif points > 21:
+    #         print('You loose')
+    #         break
+    #     else:
+    #         print('Want to get one more card?')
+    #         game(card, **deck)
 
